dcf23b8961820b95b21b34ed81b1da0d46ae8ca8
MatviyKaida <kajdamatvej@gmail.com>
2025-11-29 00:58:49 +0200

    Created signup page ui

--- frontend/src/store/UseAuthStore.js
+++ frontend/src/store/UseAuthStore.js
@@ -19,5 +19,13 @@ export const useAuthStore = create((set) => ({
         finally {
             set({isCheckingAuth:false});
         }
+    },
+    signup: async (data) => {
+        try {
+
+        }
+        catch (err) {
+            console.log(`Signup error: ${err}`);
+        }
     }
 }))

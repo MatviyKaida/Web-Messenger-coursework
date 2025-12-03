715f716c0f27e7f6450173f88841b9d59bd2ccdc
MatviyKaida <kajdamatvej@gmail.com>
2025-12-03 03:09:31 +0200

    Started working on Sidebar component

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from '../store/UseChatStore'
 
 const Sidebar = () => {
+    const { getChats, chats, selectedChat, setSelectedChat, areChatsLoading } = useChatStore();
+    useEffect(() => {
+        getChats()
+    }, [chats]);
+    if(areChatsLoading) {
+        return <SidebarSkeleton />
+    }
   return (
     <div>Sidebar</div>
   )
